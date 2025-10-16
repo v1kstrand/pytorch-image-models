5494df2748c16611e32addf1a7304cb8b035df00
v1kstrand <david.vikstrand@example.com>
2025-10-16 23:42:46 +0200

    f

--- timm/data/readers/reader_hfds_disk.py
+++ timm/data/readers/reader_hfds_disk.py
@@ -60,8 +60,6 @@ class ReaderHfdsDisk:
                 split_norm = "test"
         if split_norm not in dsd:
             raise KeyError(f"Split '{split}' not found in saved dataset. Available: {list(dsd.keys())}")
-
-        print(split_norm, dsd.keys(), len(dsd[split_norm]))
         self.ds = dsd[split_norm]
 
     def __len__(self) -> int:
