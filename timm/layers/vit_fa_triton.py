e6d34f7034c16e6e5e0ecb4f9294df1fe795623f
v1kstrand <david.vikstrand@example.com>
2025-10-29 16:02:42 +0100

    f

--- timm/layers/vit_fa_triton.py
+++ timm/layers/vit_fa_triton.py
@@ -503,19 +503,17 @@ class TritonAttention(torch.autograd.Function):
         )
                 
                 
-        """def comp(a, b):
-            diff = (a - b).abs()
-            absmax = diff.max()
-            mean = diff.mean()
-            return [absmax, mean]
+        def comp(a, b):
+            diff = (a - b).abs().to(torch.float32)
+            return torch.stack([diff.amax(), diff.mean()])
         
         max_dQ = torch.tensor(comp(dQ, gq), device=Q.device)
         max_D  = torch.tensor(comp(D, _D),  device=Q.device)
         max_M  = torch.tensor(comp(M, _M),  device=Q.device)
-        p = torch.cat((max_dQ, max_D, max_M), dim=0)"""
+        p = torch.cat((max_dQ, max_D, max_M), dim=0)
                 
         probe_out = torch.zeros(ctx.probe_size, device=Q.device, dtype=torch.float32)
-        #probe_out[:p.size(0)] = p
+        probe_out[:p.size(0)] = p
         
         return dQ, gk, gv, probe_out
     
